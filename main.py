c501ea70008f02888b8d1f71b6db3d7b806ee0fe
elenavv <89485072+elenavv@users.noreply.github.com>
2024-10-17 00:25:42 +0200

    main.py

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ import random
 import sys
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-# Удалите импорт keep_alive отсюда
-# from notmain import keep_alive
 
 token = "7332915111:AAE6-IynzV_d9JOxei6Q_W7Bm89KOptAIa8"
 dp = Dispatcher()
 
 def get_keys():
-    button = [[InlineKeyboardButton(text="Хочу анек", callback_data="anek")]]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=button)
+    button = [[types.InlineKeyboardButton(text="Хочу анек", callback_data="anek")]]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=button)
     return keyboard
 
 def get_anek():
@@ -30,12 +26,12 @@ async def answer_anek(callback: types.CallbackQuery):
     await callback.message.edit_text(text=random.choice(get_anek()), reply_markup=get_keys())
 
 async def main():
-    from notmain import keep_alive  # Переместите импорт сюда
+    from notmain import keep_alive  # Импортируем здесь, чтобы избежать циклического импорта
     keep_alive()
+    
     bot = Bot(token)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
